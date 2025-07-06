d2df393eb30ac8e600de177e44b40652bba77c25
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2025-07-05 22:19:44 -0300

    feat(repositories): add custom findById method to ProductRepository

--- src/main/java/com/german/proyecto/backend/repositories/ProductRepository.java
+++ src/main/java/com/german/proyecto/backend/repositories/ProductRepository.java
@@ -4,6 +4,9 @@ import com.german.proyecto.backend.models.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
+    Optional<ProductEntity> findById(Integer productId);
 }
